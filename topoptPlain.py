3dc4c18d664bf8c33ce8d4ad47e3179b7d253177
Bob <pnakshatrala@gmail.com>
2021-03-24 12:03:26 -0500

    Added animation.

--- topoptPlain.py
+++ topoptPlain.py
@@ -1,14 +1,18 @@
 # A 165 LINE TOPOLOGY OPTIMIZATION CODE BY NIELS AAGE AND VILLADS EGEDE JOHANSEN, JANUARY 2013
 from __future__ import division
+import math
 import numpy as np
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import spsolve
 from matplotlib import colors
 import matplotlib.pyplot as plt
+from time import sleep
 # MAIN DRIVER
 import dash
 #from jupyter_dash import JupyterDash
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+from plotly.graph_objects import Layout
 import dash_core_components as dcc
 from plotly import graph_objs as go
 import pandas as pd
@@ -65,27 +69,27 @@ app.layout = html.Div(
 													],
 													value='tr'
 										),
-									html.Div(id='force-output-container'),
+									# html.Div(id='force-output-container'),
 									html.Br(),
 									html.Br(),
 									html.Br(),
-									html.Div([html.H1('Force vector: ')], style={'display': 'inline-block', 'width': '100%'}),
+									html.Div([html.H1('Point force vector: ')], style={'display': 'inline-block', 'width': '100%'}),
                                     html.Div([html.H1('fx: ')], style={'display': 'inline-block', 'width': '25%'}),
-                                    html.Div([dcc.Input(placeholder=0.0, style={'width': 80, 'textAlign':'center'}, id='fx', min=0, max=50, step=0.1, type='number')],style={'display': 'inline-block', 'width': '25%'}),
+                                    html.Div([dcc.Input(placeholder=0.0, style={'width': 80, 'textAlign':'center'}, id='fx', min=-50, max=50, step=0.1, type='number')],style={'display': 'inline-block', 'width': '25%'}),
                                     html.Div([html.H1('fy: ')], style={'display': 'inline-block', 'width': '25%'}),
-                                    html.Div([dcc.Input(placeholder=-1.0, style={'width': 80, 'textAlign':'center'}, id='fy', min=0, max=50, step=0.1, type='number')],style={'display': 'inline-block', 'width': '25%'}),
+                                    html.Div([dcc.Input(placeholder=-1.0, style={'width': 80, 'textAlign':'center'}, id='fy', min=-50, max=50, step=0.1, type='number')],style={'display': 'inline-block', 'width': '25%'}),
 									html.Br(),
 									html.Br(),
 									html.Br(),
-									html.Div([html.H1('Boundary condition: ')], style={'display': 'inline-block', 'width': '100%'}),
+									html.Div([html.H1('Fixed boundary: ')], style={'display': 'inline-block', 'width': '100%'}),
 									dcc.Dropdown(id='bc-dropdown',
         											options=[
-														{'label': 'Top right', 'value': 'tr'},
-														{'label': 'Top left', 'value': 'tl'},
-														{'label': 'Bottom right', 'value': 'br'},
-														{'label': 'Bottom left', 'value': 'bl'}
+														{'label': 'Top', 'value': 't'},
+														{'label': 'Bottom', 'value': 'b'},
+														{'label': 'Right', 'value': 'r'},
+														{'label': 'Left', 'value': 'l'}
 													],
-													value='tr'
+													value='l'
 										),
 									html.Div(id='bc-output-container'),
                                     html.Hr(),
@@ -94,24 +98,116 @@ app.layout = html.Div(
                              ),
                     html.Div(className='eight columns div-for-charts bg-grey',
                              children=[
-                                dcc.Graph(id='timeseries-initial', config={'displayModeBar': False}), 
-								dcc.Graph(id='timeseries', config={'displayModeBar': False}, animate=True), 
-    							html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line',"maxHeight": '100%', "overflow-y": "scroll"})]),
-								dcc.Interval(id='my-interval', interval=1000)  # one tick each 5 seconds
+																dbc.Row(dbc.Col(html.Div([dcc.Graph(id='timeseries-initial', config={'displayModeBar': False})])), style={'height': '25%'}),
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																dbc.Row(dbc.Col(html.Div([dcc.Graph(id='timeseries', config={'displayModeBar': False})])), style={'height': '25%'}), 
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																html.Br(),
+																html.Br(),																																
+							    							dbc.Row(dbc.Col(html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line', 'maxHeight': '50%', 'font-size': '20px'})), style={'height': '45%'}),
+																dcc.Interval(id='my-interval', interval=1000)  # one tick each 5 seconds
                               ])
         ]
 
-)
+)])
 
 @app.callback(Output('timeseries-initial', 'figure'),
-              [Input('draw-data', 'n_clicks')], 
-              [State('nx', 'value'), State('ny', 'value')])
-def update_plot(nclicks, nx, ny):
-	fig = go.Figure(go.Scatter(x=[0,0,nx,nx,0], y=[0,ny,ny,0,0], fill="toself"))
-	#fig.update_layout(width=nx)
-	fig.update_xaxes()
-	fig.update_yaxes(    scaleanchor = "x",
-    scaleratio = 1,)
+              [Input('draw-data', 'n_clicks'), dash.dependencies.Input('bc-dropdown', 'value'), dash.dependencies.Input('force-dropdown', 'value')], 
+              [State('nx', 'value'), State('ny', 'value'), State('fx', 'value'), State('fy', 'value')])
+def update_plot(nclicks, bcloc, floc, nx, ny, fx, fy):
+
+	if nclicks == 0:
+		layout = Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
+		fig = go.Figure(go.Scatter(x=[0,0,0,0,0], y=[0,0,0,0,0], fill="toself"), layout=layout)
+		fig.update_xaxes(showgrid=False, zeroline=False)
+		fig.update_yaxes(showgrid=False, zeroline=False, scaleanchor = "x", scaleratio = 1,)
+		fig.update_layout(
+				title="Domain",
+    		legend_title="Optimal topology",
+    		font=dict(family="Courier New, monospace", size=18, color="White")
+			)
+
+		return fig
+
+
+	layout = Layout(    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)')
+	fig = go.Figure(go.Scatter(x=[0,0,nx,nx,0], y=[0,ny,ny,0,0], fill="toself"), layout=layout)
+	fig.update_xaxes(showgrid=False, zeroline=False)
+	fig.update_yaxes(showgrid=False, zeroline=False, scaleanchor = "x", scaleratio = 1,)
+	fig.update_layout(
+    title="Domain",
+    legend_title="Optimal topology",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="White"
+    		)
+		)
+
+
+	if fx == 0:
+		ay = ny + np.sign(fy)*5
+		# ax=nx
+	else:
+		theta = math.atan2(fy, fx)
+		ay = ny-10*math.sin(theta)
+		# ax = nx
+
+	theta = math.atan2(fy, fx)
+	if floc == 'tr' or floc == 'br':
+		ax = nx-10*math.cos(theta)
+	else:
+		ax = -10*math.cos(theta)
+	# ay = ny
+
+	if floc == 'tr':
+		xloc=nx
+		yloc=ny	
+	elif floc == 'tl':
+		xloc=0
+		yloc=ny	
+	elif floc == 'br':
+		xloc=nx
+		yloc=0	
+	elif floc == 'bl':
+		xloc=0
+		yloc=0	
+
+	fig.add_annotation(
+  x=xloc,  # arrows' head
+  y=yloc,  # arrows' head
+  ax=ax,  # arrows' tail
+  ay=ay,  # arrows' tail
+  xref='x',
+  yref='y',
+  axref='x',
+  ayref='y',
+  text='',  # if you want only the arrow
+  showarrow=True,
+  arrowhead=3,
+  arrowsize=1,
+  arrowwidth=2,
+  arrowcolor='red'
+	)
+
+	if bcloc == 'l':
+		fig.add_shape(type='line', x0=0, y0=0, x1=0, y1=ny+1, line=dict(color='Red',), xref='x', yref='y')
+	elif bcloc == 'r':
+		fig.add_shape(type='line', x0=nx+1, y0=0, x1=nx+1, y1=ny+1, line=dict(color='Red',), xref='x', yref='y')
+	elif bcloc == 'b':
+		fig.add_shape(type='line', x0=0, y0=0, x1=nx+1, y1=0, line=dict(color='Red',), xref='x', yref='y')
+	elif bcloc == 't':
+		fig.add_shape(type='line', x0=0, y0=ny+1, x1=nx+1, y1=ny+1, line=dict(color='Red',), xref='x', yref='y')
+
 	return fig
 
 
@@ -124,22 +220,10 @@ def callback_func(interval):
 		val = f.readlines()
 	return html.Div(val)
 
-@app.callback(
-    dash.dependencies.Output('force-output-container', 'children'),
-    [dash.dependencies.Input('force-dropdown', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
-
-@app.callback(
-    dash.dependencies.Output('bc-output-container', 'children'),
-    [dash.dependencies.Input('bc-dropdown', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
-
 @app.callback(Output('timeseries', 'figure'),
-              [Input('run-data', 'n_clicks')], 
-              [State('nx', 'value'), State('ny', 'value')])
-def update_output_run(nclicks, nx, ny):
+              [Input('run-data', 'n_clicks'), dash.dependencies.Input('force-dropdown', 'value'), dash.dependencies.Input('bc-dropdown', 'value')], 
+              [State('nx', 'value'), State('ny', 'value'), State('fx', 'value'), State('fy', 'value')])
+def update_output_run(nclicks, floc, bcloc, nx, ny, fx, fy):
 
 	ctx = dash.callback_context
 	if not ctx.triggered:
@@ -148,8 +232,6 @@ def update_output_run(nclicks, nx, ny):
 		button_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
 	if (nclicks or 0) and (button_id == "run-data"):
-		#df = pd.read_json(jsonified_cleaned_data, orient='split')
-		#df['Produce'] = 1
 		nelx=nx
 		nely=ny
 		volfrac=0.4
@@ -157,35 +239,33 @@ def update_output_run(nclicks, nx, ny):
 		penal=3.0
 		ft=1 # ft==0 -> sens, ft==1 -> dens
 		f = open('filename.txt', 'w+')
-		xPhys = main(nelx,nely,volfrac,penal,rmin,ft,f)
-		#print(type(xPhys))
+		xPhys = main(nelx,nely,volfrac,penal,rmin,ft,floc,fx,fy,bcloc,f)
 		f.close()
-		# df = px.data.gapminder().query("country=='Canada'")
-		# fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
-		# return fig
-
-		img_rgb = np.array([[[255, 0, 0], [0, 255, 0], [0, 0, 255]],
-                    [[0, 255, 0], [0, 0, 255], [255, 0, 0]]
-                   ])
 		fig = px.imshow(xPhys.reshape((nelx, nely)).T)
-		# fig = px.imshow(img_rgb)
+		# fig.update_layout(
 		return fig
 
 	else:
-		return {'data' : [], 'layout' : go.Layout(
-                        template='plotly_dark',
-                        paper_bgcolor='rgb(233,233,233)',
-                        plot_bgcolor='rgb(233,233,233)',
-                        margin={'b': 15},
-                        hovermode='x',
-                        autosize=True,
-                        title={'text': 'Topology optimization', 'font': {'color': 'black', 'size': 30}, 'x': 0.5},
-                )}
-		# fig = go.Figure(go.Scatter(x=[0,0,180,180,0], y=[0,60,60,0,0], fill="toself"))
-		# return fig
 
+		layout = Layout(    paper_bgcolor='rgba(0,0,0,0)',
+    										plot_bgcolor='rgba(0,0,0,0)')
 
-def main(nelx,nely,volfrac,penal,rmin,ft, fout):
+		fig = go.Figure(go.Scatter(x=[0,0,0,0,0], y=[0,0,0,0,0], fill="toself"), layout=layout)
+		fig.update_xaxes(showgrid=False, zeroline=False)
+		fig.update_yaxes(showgrid=False, zeroline=False, scaleanchor = "x", scaleratio = 1,)
+		fig.update_layout(
+    title="Plot Title",
+    legend_title="Optimal topology",
+    font=dict(
+        family="Courier New, monospace",
+        size=18,
+        color="White"
+    		)
+		)
+
+		return fig
+
+def main(nelx,nely,volfrac,penal,rmin,ft,floc,fx,fy,bcloc,fout):
 	myGlobalStr = ""
 	print("Minimum compliance problem with OC")
 	print("ndes: " + str(nelx) + " x " + str(nely))
@@ -240,26 +320,65 @@ def main(nelx,nely,volfrac,penal,rmin,ft, fout):
 	Hs=H.sum(1)
 	# BC's and support
 	dofs=np.arange(2*(nelx+1)*(nely+1))
-	fixed=np.union1d(dofs[0:2*(nely+1):2],np.array([2*(nelx+1)*(nely+1)-1]))
+	# fixed=np.union1d(dofs[0:2*(nely+1):2],np.array([2*(nelx+1)*(nely+1)-1]))
+	# fixed=dofs[0:2*(nely+1):2]
+	# print("Original fixed = ", fixed)
+	fixed=[]
+	if bcloc == 'l':
+		for i in range(nely+1):
+			ind = 2*i
+			fixed.append(dofs[ind])
+			fixed.append(dofs[ind+1])
+	elif bcloc == 'r':
+		for i in range(nely+1):
+			ind = 2*i + 2*(nelx)*(nely+1)
+			fixed.append(dofs[ind])
+			fixed.append(dofs[ind+1])
+	elif bcloc == 't':
+		for i in range(nelx+1):
+			ind = 2*i*(nely+1) 
+			fixed.append(dofs[ind])
+			fixed.append(dofs[ind+1])
+	elif bcloc == 'b':
+		for i in range(nelx+1):
+			ind = (nely+1) + 2*i*(nely+1) 
+			fixed.append(dofs[ind])
+			fixed.append(dofs[ind+1])
+	else:
+		fixed=np.union1d(dofs[0:2*(nely+1):2],np.array([2*(nelx+1)*(nely+1)-1]))
+	print("new fixed = ", fixed)
+
 	free=np.setdiff1d(dofs,fixed)
 	# Solution and RHS vectors
 	f=np.zeros((ndof,1))
 	u=np.zeros((ndof,1))
 	# Set load
-	f[1,0]=-1
+	if floc == 'tr':
+		f[2*(nelx)*(nely+1),0]=fx # tr
+		f[2*(nelx)*(nely+1)+1,0]=fy # tr
+	elif floc == 'br':
+		f[2*(nelx+1)*(nely+1)-2,0]=fx # br
+		f[2*(nelx+1)*(nely+1)-1,0]=fy # br
+	elif floc == 'tl':
+		f[0,0]=fx # tl
+		f[1,0]=fy # tl
+	elif floc == 'bl':
+		f[2*(nely+1)-2,0]=fx # bl
+		f[2*(nely+1)-1,0]=fy # bl
+	else:
+		f[1,0]=-1 # tl
 	# Initialize plot and plot the initial design
-	# plt.ion() # Ensure that redrawing is possible
-	# fig,ax = plt.subplots()
-	# im = ax.imshow(-xPhys.reshape((nelx,nely)).T, cmap='gray',\
-	# interpolation='none',norm=colors.Normalize(vmin=-1,vmax=0))
-	# fig.show()
-   	# Set loop counter and gradient vectors 
+  # Set loop counter and gradient vectors 
 	loop=0
 	change=1
 	dv = np.ones(nely*nelx)
 	dc = np.ones(nely*nelx)
 	ce = np.ones(nely*nelx)
-	while change>0.01 and loop<200:
+	TotalLoops = 100
+	nPrint = 20
+	PrintFreq = int(TotalLoops/nPrint)
+	print("PrintFreq = ", PrintFreq)
+	while change>0.001 and loop<TotalLoops:
 		loop=loop+1
 		# Setup and solve FE problem
 		sK=((KE.flatten()[np.newaxis]).T*(Emin+(xPhys)**penal*(Emax-Emin))).flatten(order='F')
@@ -287,9 +406,6 @@ def main(nelx,nely,volfrac,penal,rmin,ft, fout):
 		elif ft==1:	xPhys[:]=np.asarray(H*x[np.newaxis].T/Hs)[:,0]
 		# Compute the change by the inf. norm
 		change=np.linalg.norm(x.reshape(nelx*nely,1)-xold.reshape(nelx*nely,1),np.inf)
-		# Plot to screen
-		# im.set_array(-xPhys.reshape((nelx,nely)).T)
-		# fig.canvas.draw()
 		# Write iteration history to screen (req. Python 2.6 or newer)
 		print("it.: {0} , obj.: {1:.3f} Vol.: {2:.3f}, ch.: {3:.3f}".format(\
 					loop,obj,(g+volfrac*nelx*nely)/(nelx*nely),change))
@@ -297,12 +413,11 @@ def main(nelx,nely,volfrac,penal,rmin,ft, fout):
 		volVal = "%.2f" % ((g+volfrac*nelx*nely)/(nelx*nely))
 		changeVal = "%.2f" % change
 		myGlobalStr = "iteration : {it} , \t objective: {obj}, \t Volume fraction: {vol}, \t change in volume: {change}".format(it=loop,obj=objVal, vol=volVal, change=changeVal)
-		print(myGlobalStr, file=fout, flush=True)
-		# print("yoyoma", file=f, flush=True)
+		if loop % PrintFreq == 0:
+			print(myGlobalStr, file=fout, flush=True)
 	
 	return xPhys
-	# Make sure the plot stays and that the shell remains	
-	# plt.show()
+
 #element stiffness matrix
 def lk():
 	E=1
@@ -336,20 +451,6 @@ def oc(nelx,nely,x,volfrac,dc,dv,g):
 # The real main driver    
 if __name__ == "__main__":
 	# # Default input parameters
-	# nelx=180
-	# nely=60
-	# volfrac=0.4
-	# rmin=5.4
-	# penal=3.0
-	# ft=1 # ft==0 -> sens, ft==1 -> dens
-	# import sys
-	# if len(sys.argv)>1: nelx   =int(sys.argv[1])
-	# #if len(sys.argv)>2: nely   =int(sys.argv[2])
-	# #if len(sys.argv)>3: volfrac=float(sys.argv[3])
-	# #if len(sys.argv)>4: rmin   =float(sys.argv[4])
-	# #if len(sys.argv)>5: penal  =float(sys.argv[5])
-	# #if len(sys.argv)>6: ft     =int(sys.argv[6])
-	# #main(nelx,nely,volfrac,penal,rmin,ft)
 	f = open('filename.txt', 'w+')
 	f.close()
-	app.run_server(debug=True, port=8050)
+	app.run_server(debug=False, port=8050)
